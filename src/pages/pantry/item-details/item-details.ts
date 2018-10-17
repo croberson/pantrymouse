810324c934aa25b35d00631176d304d101978760
Christopher Roberson <chris.roberson@coxautoinc.com>
2018-10-17 08:57:41 -0500

    remove functionality added.  Update/cancel buttons on details swapped.

--- src/pages/pantry/item-details/item-details.ts
+++ src/pages/pantry/item-details/item-details.ts
@@ -2,8 +2,8 @@ import {Component} from '@angular/core';
 import {ViewController} from 'ionic-angular';
 import {NavParams} from 'ionic-angular';
 import {PantryItem} from '../../../lib/models/pantry_item_model';
-import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
-import { ToastController } from 'ionic-angular';
+import {SQLite, SQLiteObject} from '@ionic-native/sqlite';
+import {ToastController, AlertController} from 'ionic-angular';
 
 
 @Component({
@@ -18,7 +18,8 @@ export class ItemDetailsPage {
     constructor(public viewCtrl: ViewController,
                 public navParams: NavParams,
                 private sqlite: SQLite,
-                public toastCtrl: ToastController) {
+                public toastCtrl: ToastController,
+                private alertCtrl: AlertController) {
 
         this.mainItem = this.navParams.get('item');
         this.item = new PantryItem();
@@ -53,38 +54,97 @@ export class ItemDetailsPage {
             name: 'data.db',
             location: 'default'
         })
-        .then((db: SQLiteObject) => {
-            db.executeSql(qry, values)
-                .then(res => {
-                    console.log(res);
-                    const toast = this.toastCtrl.create({
-                        message: 'Item updated.',
-                        duration: 5000
-                    });
-                    toast.present();
-                    this.item.badgeColorChange();
-                    this.mainItem.copy(this.item);
-                    this.close();
+            .then((db: SQLiteObject) => {
+                db.executeSql(qry, values)
+                    .then(res => {
+                        console.log(res);
+                        const toast = this.toastCtrl.create({
+                            message: 'Item updated.',
+                            duration: 5000
+                        });
+                        toast.present();
+                        this.item.badgeColorChange();
+                        this.mainItem.copy(this.item);
+                        this.close();
 
-                })
-                .catch(e => {
-                    console.log(e);
-                    const toast = this.toastCtrl.create({
-                        message: e.message,
-                        duration: 5000
+                    })
+                    .catch(e => {
+                        console.log(e);
+                        const toast = this.toastCtrl.create({
+                            message: e.message,
+                            duration: 5000
+                        });
+                        toast.present();
                     });
-                    toast.present();
-                });
-        }).
-        catch(e => {
+            }).catch(e => {
             console.log(e);
             const toast = this.toastCtrl.create({
                 message: e.message,
                 duration: 5000
             });
 
-        toast.present();
+            toast.present();
         });
+    }
+
+    public removeFromPantry() : boolean {
+        let alert = this.alertCtrl.create({
+            title: 'Confirm remove',
+            message: 'Are you sure you want to remove this item from your pantry?',
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    handler: () => {
+                    }
+                },
+                {
+                    text: 'Remove',
+                    handler: () => {
+                        let qry: string = `DELETE FROM pantry_items WHERE id = ?`;
+                        let values: any[] = [this.item.id];
+
+                        this.sqlite.create({
+                            name: 'data.db',
+                            location: 'default'
+                        }).then((db: SQLiteObject) => {
+                            db.executeSql(qry, values)
+                                .then(res => {
+                                    console.log("1---");
+                                    const toast = this.toastCtrl.create({
+                                        message: 'Item removed from pantry.',
+                                        duration: 5000
+                                    });
+                                    toast.present();
+
+                                })
+                                .catch(e => {
+                                    console.log(e);
+                                    const toast = this.toastCtrl.create({
+                                        message: e.message,
+                                        duration: 5000
+                                    });
+                                    toast.present();
+                                });
+                            console.log("2---");
+                            }).then(() => {
+                            console.log("3---");
+                        }).catch(e => {
+                            console.log(e);
+                            const toast = this.toastCtrl.create({
+                                message: e.message,
+                                duration: 5000
+                            });
+
+                            toast.present();
+                        });
+                    }
+                }
+            ]
+        });
+        alert.present();
+
+        return false;
     }
 
     changeNum(operation: string, valueName: string) {
